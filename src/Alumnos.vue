d8266d7901c3c6106f789b24edf69701a1bbc535
Luis Ballesteros <33975183+lubahez@users.noreply.github.com>
2018-04-28 11:15:44 -0500

    Add files via upload

---
+++ src/Alumnos.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="row">
+		<div class="col-md-12">
+			<form class="form-inline">
+				<div class="form-group">
+				   <label for="exampleInputName2">Nombre:</label>
+				   <input type="text" class="form-control" v-model="nuevo.nombre" id="exampleInputName2" placeholder="Jane Doe">
+				</div>
+				<div class="form-group">
+				  <label for="exampleInputEmail2">Apellidos:</label>
+				  <input type="email" class="form-control" v-model="nuevo.apellidos" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+				</div>
+				<button class="btn btn-success" v-on:click="agregar">Agregar</button>
+			</form>
+		</div>
+		<div class="col-md-12">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Nombre</th>
+						<th>Apellidos</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="(alumno, index) in alumnos">
+						<td>{{ alumno.nombre }}</td>
+						<td>{{ alumno.apellidos }}</td>
+						<td>
+							<button class="btn btn-info" v-on:click="editar">Editar</button>
+							<button class="btn btn-warning" v-on:click="remover(index)">Remover</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'alumnos',
+	data () {
+		return {
+			nuevo: {},
+			alumnos:[
+				{ nombre:'Fulanito', apellidos:'Ricardo' },
+				{ nombre:'Chuchita', apellidos:'Bolsón' }
+			]
+		}
+	},
+
+	methods: {
+
+		agregar: function () {
+			this.alumnos.push(this.nuevo);
+			this.nuevo = {};
+		},
+
+		editar: function () {
+			console.log('Editar');
+		},
+
+		remover: function (alumno) {
+			this.alumnos.splice(alumno, 1);
+		}
+	}
+}
+</script>
---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <img src="./assets/logo.png" width="70px">
+        </div>
+        <div class="col-md-9">
+          <h1>{{ msg }}</h1>
+          <hr>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <h4>Menú</h4>
+          <ul>
+            <li><a href="">Alumnos</a></li>
+            <li><a href="">Maestros</a></li>
+            <li><a href="">Grupos</a></li>
+            <li><a href="">Registro asistencia</a></li>
+          </ul>
+        </div>
+        <div class="col-md-9">
+          <alumnos></alumnos>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import alumnos from './Alumnos.vue'
+
+export default {
+  name: 'app',
+  components: { alumnos },
+  data () {
+    return {
+      msg: 'Aplicación de control de asistencia 1.0',
+      alumnos:[],
+      maestros:[],
+      grupos:[],
+      items: [
+        { text:'1ko' },
+        { text:'2ko' },
+        { text:'3ko' },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
